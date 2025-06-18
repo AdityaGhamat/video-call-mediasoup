4349602ca20c81a9cc235e46137fef6c4045b41f
AdityaGhamat <adityaghamat01@gmail.com>
2025-06-18 15:59:27 +0530

    initial setup of server is completed.

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import fs from "fs"; // we need to read it to keys
+import https from "https"; // we need it for secures express server
+import express from "express";
+import { Server } from "socket.io";
+import mediasoup from "mediasoup";
+import { configmadhe } from "./config/random.js";
+
+const app = express();
+app.use(express.static("public"));
+const key = fs.readFileSync("./src/config/cert.key");
+const cert = fs.readFileSync("./src/config/cert.crt");
+const options = { key, cert };
+const httpsServer = https.createServer(options, app);
+const io = new Server(httpsServer, {
+  cors: {
+    origin: ["https://localhost:3030"],
+  },
+});
+httpsServer.listen(3030);
